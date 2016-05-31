077fb2d34580824418bb96af2337505996960a98
dell <dell@IDELIVER23>
2016-05-31 18:25:12 +0530

    Add project files.

---
+++ LeanFtTestProject2/LeanFtTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HP.LFT.SDK;
+using HP.LFT.Verifications;
+using HP.LFT.SDK.Web;
+
+namespace LeanFtTestProject1
+{
+    [TestClass]
+    public class LeanFtTest : UnitTestClassBase<LeanFtTest>
+    {
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            GlobalSetup(context);
+        }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        // launching browser
+            HP.LFT.SDK.Web.IBrowser OBrowser = BrowserFactory.Launch(BrowserType.InternetExplorer);
+           
+            OBrowser.Navigate("Google.com");  //nevigate to url
+            OBrowser.Sync();
+            // OBrowser.CloseAllTabs(); close all opened tabs
+
+
+            OBrowser.Describe<ILink>(new LinkDescription
+            {
+                TagName = @"A",
+                InnerText = @"Gmail"
+            });
+          
+
+
+            
+        }
+
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            GlobalTearDown();
+        }
+    }
+}
